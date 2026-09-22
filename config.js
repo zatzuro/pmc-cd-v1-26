eed1263f3f233c60b6fdf1cbd2910d191f27d0dc
zatzuro <zatzuro@gmail.com>
2026-09-22 13:28:03 -0500

    Conectar frontend con API V1.0

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.PRISA_CALENDAR_CONFIG = {
-  API_URL: "",
+  API_URL: "https://script.google.com/macros/s/AKfycbwd29DAfHveG_BPgXKNxfErri5dRLQHcXCPELr0qPuvr8MvoHcwt79EWXXUYjmsbMg/exec",
   PILOT_START: "2026-09-01",
   PILOT_END: "2026-12-31"
 };
